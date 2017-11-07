e1b34e2540f07f82e37c863e4367eb9056b6181e
PJHutson <31662048+PJHutson@users.noreply.github.com>
2017-11-07 11:11:45 -0600

    Add files via upload

---
+++ 12.1.py
@@ -0,0 +1,11 @@
+def main():
+    n(5)            # set max to 5
+
+
+def n(count):           # define count
+    num = 0             # set initial to 0
+    while num < count:
+        num += 1           # rearrange first to 1
+        print(num)          # print number
+
+main()
