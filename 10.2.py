3254a6b448736a64870e0874b28db472854bec97
PJHutson <31662048+PJHutson@users.noreply.github.com>
2017-11-28 11:06:49 -0600

    Add files via upload

--- 10.2.py
+++ 10.2.py
@@ -1,7 +1,7 @@
-import questions
+import questions        # importing questions file
 
 
-def main():
+def main():         # make questions
     q1 = questions.Question("What is 2+2?", "A. fish", "B. 5", "C. 4", "D. All of the above", "C")
     q2 = questions.Question("What is the best food ever created?", "A. pizza", "B. sushi", "C. cheeseburger", "D. Spaghetti", "A")
     q3 = questions.Question("What is the capital of Illinois?", "A. chicago", "B. crystal lake", "C. springfield", "D. nashville", "C")
@@ -12,14 +12,13 @@ def main():
     q8 = questions.Question("What country touches the USA?", "A. Russia", "B. Thailand", "C. Canada", "D. Brazil", "C")
     q9 = questions.Question("Who did Trump beat for the presidency?", "A. Clinton", "B. The Rock", "C. Romney", "D. Obama", "A")
     q10 = questions.Question("What is Meijer?", "A. A food brand", "B. A school", "C. A computer", "D. A retail store", "D")
-
-    player1 = 0
+    player1 = 0     # initial scores set to 0
     player2 = 0
 
-    set_1 = [q1, q3, q5, q7, q9]
+    set_1 = [q1, q3, q5, q7, q9]        # sets of questions
     set_2 = [q2, q4, q6, q8, q10]
 
-    print("player1: ")
+    print("player1: ")      # giving player 1 set 1
     for query in set_1:
         print("\n")
         print(query.get_question())
@@ -27,12 +26,11 @@ def main():
         print(query.get_a2())
         print(query.get_a3())
         print(query.get_a4())
-        quess = input("Please enter the letter for your answer: ")
-        if guess.upper == query.get_answer():
-            print("You're right!")
+        guess = input("Please enter the letter for your answer: ").upper().strip(" ")
+        if guess == query.get_answer():
+            print("You're right!")          # if correct then print so
             player1 += 1
-        print("Player 1 earned:", + str(player1) + "points.")
-
+            print("Player 1 earned:", str(player1), "point(s).")
     print("player2: ")
     for query in set_2:
         print("\n")
@@ -41,11 +39,10 @@ def main():
         print(query.get_a2())
         print(query.get_a3())
         print(query.get_a4())
-        quess = input("Please enter the letter for your answer: ")
-        if guess.upper == query.get_answer():
+        guess = input("Please enter the letter for your answer: ").upper().strip(" ")
+        if guess == query.get_answer():
             print("You're right!")
             player2 += 1
-        print("Player 2 earned:", + str(player2) + "points.")
-
+    print("Player 2 earned:", str(player2), "point(s).")
 
 main()
